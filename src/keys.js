969aca65427f0a44942830f4eb820e1e0c52c80a
Harris Kwong <harriskwong120898@gmail.com>
2024-05-12 14:47:47 -0400

    Configure app

---
+++ src/keys.js
@@ -0,0 +1,10 @@
+const keys = {
+  apiKey: "AIzaSyDyyJIlmY75Q8aZFDuQWl21El21IR_bwLw",
+  authDomain: "demostudysync.firebaseapp.com",
+  projectId: "demostudysync",
+  storageBucket: "demostudysync.appspot.com",
+  messagingSenderId: "549614547280",
+  appId: "1:549614547280:web:133bf22eaeeb5996767c55",
+};
+
+export default keys;
